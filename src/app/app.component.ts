546deb0f3736b0bec2e9aae11a4f78e88407e294
eric nicolas <eric.e.nicolas@gmail.com>
2018-06-13 16:18:09 -0700

    netflix updated half of component 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  
+
+  select(event){
+    console.log(event);
+    let nid : Number = event.path[1].id;
+    let array: NodeListOf<Element> = document.getElementsByClassName("midicons");
+    console.log(array);
+    for(var i=0; i< array.length;i++){
+      if( nid == parseInt(array[i].id)){
+        array[i].lastElementChild.className = "text selected";
+      }
+      else{
+        array[i].lastElementChild.className = "text nonselected";
+      }
+  }
+}
 }
