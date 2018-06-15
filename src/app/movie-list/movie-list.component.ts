30ec2fa296bbdfe547f0ca2e4eba8be26de3f805
1st <eric.e.nicolas@gmail.com>
2018-06-15 15:00:43 -0700

    movie list added

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -8,15 +8,17 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-  topmovies=this.getmovies();
+  topmovies= null;
   constructor(private http: Http) { }
 
   ngOnInit() {
-    
+  this.getmovies();
   }
   getmovies(){
+  console.log( this.http.get("https://api.themoviedb.org/3/movie/top_rated?page=1&language=en-US&api_key=f509dad62270f06d7624a7de2b25b127"));
   this.http.get("https://api.themoviedb.org/3/movie/top_rated?page=1&language=en-US&api_key=f509dad62270f06d7624a7de2b25b127").subscribe(response => {
-    this.topmovies = response.json();
+    this.topmovies = response.json().results;
+    console.log(this.topmovies[0].id);
     console.log(this.topmovies);
 });
   }
